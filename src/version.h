0ef7a425dcb8cfbb4afb96c573bea897047721be
Arjan Dekkers <dekkersarjan@hotmail.com>
2016-10-15 05:21:43 +0200

    bump version and protocol

--- src/version.h
+++ src/version.h
@@ -42,7 +42,7 @@ static const int DATABASE_VERSION = 70501;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70002;
+static const int PROTOCOL_VERSION = 70004;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 // static const int MIN_PROTO_VERSION = 209;
@@ -51,7 +51,7 @@ static const int PROTOCOL_VERSION = 70002;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70001;
+static const int MIN_PEER_PROTO_VERSION = 70003;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
